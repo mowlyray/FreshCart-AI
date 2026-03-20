33155de1b391c43dc57aa215932d97fc140be2b0
mowlyray <mowlyray421@gmail.com>
2026-03-20 20:07:21 +0600

    start authentiction

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,45 @@
 import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+import { async } from './../.next/dev/types/routes.d';
+import connectDb from "./lib/db";
+import bcrypt from "bcryptjs";
+import User from "./models/user.model";
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "Enter your email" },
+        password: { label: "Password", type: "password", placeholder: "Enter your password" },
+      },
+      //connect db
+      async authorize(credintials, request) {
+      
+          await connectDb()
+          const email=credintials.email
+          const password=credintials.password as string
+          const user=await User.findOne({email})
+          if(!user){
+            throw new Error("user does not exist")
+          }
+          const isMatch=await bcrypt.compare(password,user.password)
+          if(!isMatch){
+            throw new Error("incorrect password")
+          }
+          return {
+            id:user._id,
+            name:user.name,
+            email:user.email,
+            role:user.role
+          }
+      },
+
+    })
+  ],
 })
+
+//sign in step
+//connect db
+//email check
+//password match
+//return user data and token
