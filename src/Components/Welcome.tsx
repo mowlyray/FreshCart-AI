a7fad5268036b1f80193b1cc73acef5bb806760f
mowlyray <mowlyray421@gmail.com>
2026-03-21 16:40:46 +0600

    welcome page create

--- src/Components/Welcome.tsx
+++ src/Components/Welcome.tsx
@@ -1,8 +1,18 @@
+'use client'
 import React from 'react'
+import { motion } from 'motion/react'
+import { ShoppingBasket } from 'lucide-react'
 
 function Welcome() {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center min-h-screen text-center p-6'>
+        <motion.div
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 2 }}>
+            <h1 className='text-4xl md:text-5xl font-extrabold text-green-700'>Welcome to Freshcart AI</h1>
+            <ShoppingBasket />
+            hello</motion.div>
       
     </div>
   )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="w-full min-h-screen bg-linear-to-b from-green-100 to-white">
         {children}
       </body>
     </html>
