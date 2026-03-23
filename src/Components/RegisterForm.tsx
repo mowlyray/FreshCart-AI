ad729560d3c1bde3f52c213419fbd91c886aca44
mowlyray <mowlyray421@gmail.com>
2026-03-23 22:54:21 +0600

    RegisterPage

--- src/Components/RegisterForm.tsx
+++ src/Components/RegisterForm.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function RegisterForm() {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center min-h-screen px-6 py-10 bg-white relative'>
+      <div className=''>
+
+      </div>
       
     </div>
   )
