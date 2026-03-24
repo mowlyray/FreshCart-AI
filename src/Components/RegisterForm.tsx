ee3fe3b5d1f0e35686e93f616c465bb560a6deec
mowlyray <mowlyray421@gmail.com>
2026-03-24 16:44:10 +0600

    create registerpage input email password and button

--- src/Components/RegisterForm.tsx
+++ src/Components/RegisterForm.tsx
@@ -1,12 +1,18 @@
-import { ArrowLeft } from 'lucide-react'
-import React from 'react'
+import { ArrowLeft, EyeIcon, EyeOff, Key, Leaf, Lock, Mail, User } from 'lucide-react'
+import React, { useState } from 'react'
 import { motion } from 'motion/react'
+import Register from './../app/register/page';
 
 type propType={
     previousStep:(s:number)=>void
 }
 
 function RegisterForm({previousStep}:propType) {
+  const [name,setName] = useState("")
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+  const [showPassword,setShowPassword] = useState(false)
+
   return (
     <div className='flex flex-col items-center justify-center min-h-screen px-6 py-10 bg-white relative'>
       <div className='absolute top-6 left-6 flex items-center gap-2 text-green-700 hover:text-green-800 ransition-colors cursor-pointer'
@@ -14,7 +20,66 @@ function RegisterForm({previousStep}:propType) {
         <ArrowLeft className='w-5 h-5'/>
         <span className='font-medium'>Back</span> 
       </div>
-      <motion.h1>Create Account</motion.h1>
+      <motion.h1
+      initial={{
+        y:-10,
+        opacity:0
+      }}
+      animate={{
+        y:0,
+        opacity:1
+      }}
+      transition={{
+        duration:0.6
+      }}
+       className='text-4xl font-extrabold text-green-700 mb-2'>Create Account</motion.h1>
+       <p className='text-gray-600 mb-8 flex items-center'>Join Freshcart AI today <Leaf className='w-5 h-5 text-green-600'/></p>
+       <motion.form initial={{
+        opacity:0
+      }}
+      animate={{
+        opacity:1
+      }}
+      transition={{
+        duration:0.6
+      }}
+      className='flex flex-col gap-5 w-full max-w-sm'>
+
+        <div className='relative'>
+          <User className='absolute left-3 top-3.5 w-5 h-5 text-gray-400'/>
+          <input type='text' placeholder='Your Name' className='w-full border border-gray-300 rounded-xl py-3 pl-10 pr-4 text-gray-800 focus:outline-none focus:ring-2 focus:ring-green-500'
+          onChange={(e) => setName(e.target.value)} value={name}/>
+          
+        </div>
+
+        <div className='relative'>
+          <Mail className='absolute left-3 top-3.5 w-5 h-5 text-gray-400'/>
+          <input type='text' placeholder='Your Email' className='w-full border border-gray-300 rounded-xl py-3 pl-10 pr-4 text-gray-800 focus:outline-none focus:ring-2 focus:ring-green-500'
+          onChange={(e) => setEmail(e.target.value)} value={email}/>
+          
+        </div>
+
+        <div className='relative'>
+          <Lock className='absolute left-3 top-3.5 w-5 h-5 text-gray-400'/>
+          <input type={showPassword ? 'text' : 'password'} placeholder='Your password' className='w-full border border-gray-300 rounded-xl py-3 pl-10 pr-4 text-gray-800 focus:outline-none focus:ring-2 focus:ring-green-500'
+          onChange={(e) => setPassword(e.target.value)} value={password}/>
+          {
+            showPassword ? <EyeOff className='absolute right-3 top-3.5 w-5 h-5 text-gray-500 cursor-pointer' onClick={()=>setShowPassword(false)}/> : <EyeIcon className='absolute right-3 top-3.5 w-5 h-5 text-gray-500 cursor-pointer' onClick={()=>setShowPassword(true)}/>
+          }
+        </div>
+        {
+          (()=>{
+            const formValidation = name!=="" && email!=="" && password!==""
+            return <button className={`w-full font-semibold py-3 rounded-xl shadow-md transition-all duration-200  ${formValidation ? 'bg-green-600 hover:bg-green-700 text-white cursor-pointer' : 'bg-gray-300 text-gray-500 cursor-not-allowed'}`}>
+              Register
+
+            </button>
+          })()
+        }
+
+
+
+       </motion.form>
       
     </div>
   )
