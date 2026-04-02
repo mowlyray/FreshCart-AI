356f18fea776aaaa30d053cde95250d9c4009df9
mowlyray <mowlyray421@gmail.com>
2026-04-02 20:20:39 +0600

    add middleware

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,8 +1,28 @@
+import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
-export function proxy(req:NextRequest) {
+export async function proxy(req:NextRequest) {
 
     const {pathname} = req.nextUrl
     const publicRoutes = ["/login","/register","api/auth","/favicon.ico","/_next"]
 
+    if(publicRoutes.some((path) => pathname.startsWith(path))){
+        return NextResponse.next()
+    }
+
+    const token = await getToken({req,secret:process.env.AUTH_SECRET})
+    console.log("Token in middleware",token)
+    console.log("Request URL in middleware",req.url)
+
+    if(!token){
+        const loginUrl = new URL("/login",req.url)
+        loginUrl.searchParams.set("callbackUrl",req.url)
+        return NextResponse.redirect(loginUrl)
+    }
+    return NextResponse.next()
+
 }
+
+
+
+//req......middleware.....server
