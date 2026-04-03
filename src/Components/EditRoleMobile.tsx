af26a008e83109affd84713c6e5bedbb80c38cf8
mowlyray <mowlyray421@gmail.com>
2026-04-03 21:18:15 +0600

    creating edit role and mobile page

--- src/Components/EditRoleMobile.tsx
+++ src/Components/EditRoleMobile.tsx
@@ -1,9 +1,83 @@
-import React from 'react'
+'use client'
+import { ArrowRight, Bike, User, UserCog } from 'lucide-react'
+import { motion } from 'motion/react'
+import React, { useState } from 'react'
 
 function EditRoleMobile() {
+
+  const [roles,setRoles]= useState([
+    {id:"admin",label:"Admin",icon:UserCog},
+    {id:"user",label:"User",icon:User},
+    {id:"deliveryBoy",label:"Delivery Boy",icon:Bike}
+  ])
+
+  const [selectedRole,setSelectedRole]=useState("")
+  const [mobile,setMobile]=useState("")
+
   return (
-    <div className='flex flex-col min-h-screen p-6 w-full bg-white'>
-      hello
+    <div className='flex flex-col items-center min-h-screen p-6 w-full '>
+      <motion.h1 
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+        className='text-3xl md-text-4xl font-extrabold text-green-700 text-center mt-8'
+        >Select Your Role</motion.h1>
+
+        <div className='flex flex-col md:flex-row justify-center items-center gap-6 mt-10'>
+          {
+            roles.map((role) => {
+              const Icon = role.icon
+              const isSelected = selectedRole == role.id
+              return (
+                
+                <motion.div 
+                key={role.id}
+                whileTap={{scale:0.96}}
+                onClick={() => setSelectedRole(role.id)}
+
+                className={`flex flex-col items-center justify-center w-48 h-44 rounded-2xl border-2 transition-all cursor-pointer ${isSelected ? 'border-green-600 bg-green-100 shadow-lg' : 'bg-white hover:border-green-400 border-gray-300'}`}
+                >
+                  <Icon />
+                  <span>{role.label}</span>
+                </motion.div>
+              )
+            })
+          }
+        </div>
+
+        <motion.div 
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.5,
+         duration: 0.6 }}
+         className='flex flex-col items-center mt-10'>
+          <label htmlFor="mobile" className='text-gray-700 font-medium mb-2'>Enter Your Mobile Number</label>
+
+          <input 
+          type="tel" 
+          id="mobile" 
+          className='w-64 md:w-80 px-4 py-3 rounded-xl border border-gray-300 focus:ring-2 focus:ring-green-500 focus:outline-none text-gray-800' placeholder='eg. 01....'
+          onChange={(e) => setMobile(e.target.value)}
+          />
+        </motion.div>
+        <motion.button
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.7 }}
+
+        disabled={mobile.length!==11 || !selectedRole}
+
+        className={`inline-flex items-center gap-2 font-semibold py-3 px-8 rounded-2xl shadow-md transition-all
+        duration-200 w-[200px] mt-10 ${
+          selectedRole && mobile.length === 11 
+          ? 'bg-green-600 hover:bg-green-700 text-white cursor-pointer' 
+          : 'bg-gray-300 text-gray-500 cursor-not-allowed'
+        }`}
+        >
+          Go to Home
+          <ArrowRight />
+
+        </motion.button>
     </div>
   )
 }
--- src/Components/Welcome.tsx
+++ src/Components/Welcome.tsx
@@ -13,7 +13,7 @@ function Welcome({nextStep}:propType) {
         <motion.div
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1 }} className='flex items-center'>
+        transition={{ duration: 0.6 }} className='flex items-center gap-3'>
 
             <ShoppingBasket className='w-10  h-20 text-green-600'/>
             <h1 className='text-4xl md:text-5xl font-extrabold text-green-700'>Welcome to Freshcart AI</h1>
