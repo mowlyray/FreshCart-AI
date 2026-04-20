c191e4c7532174dc10e73c0f0f454fc49ee2bba8
mowlyray <mowlyray421@gmail.com>
2026-04-20 23:25:55 +0600

    create log out button

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,8 +1,10 @@
-import { Search, ShoppingCartIcon, User } from 'lucide-react';
+"use client"
+import { LogOut, Package, Search, ShoppingCartIcon, User } from 'lucide-react';
 import mongoose from 'mongoose';
 import Link from 'next/link';
 import React, { useState } from 'react'
 import Image from 'next/image';
+import { AnimatePresence, motion } from 'framer-motion';
 
 interface IUser {
   _id?: mongoose.Types.ObjectId;
@@ -31,15 +33,52 @@ function Nav({user}:{user:IUser}) {
       </form>
 
       <div className='flex items-center gap-3 md:gap-6 relative'>
-        <Link href={''} className='relative bg-white rounded-full w-11 h--11 flex items-center justify-center shadow-md hover:scale-105 transition'>
+        <Link href={''} className='relative bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition'>
         <ShoppingCartIcon className='text-green-600 w-6 h-10'/>
         <span className='absolute -top-1 -right-1 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full font-semibold shadow'>0</span>
         </Link>
-
-        <div className='bg-white rounded-full w-11 h-11 flex items-center justify-center overflow-hidden shadow-md hover:scale-105 transition-transform relative'>
+        
+        <div className='relative'>
+          <div className='bg-white rounded-full w-11 h-11 flex items-center justify-center overflow-hidden shadow-md hover:scale-105 transition-transform cursor-pointer' onClick={()=>setOpen(prev=>!prev)}>
           {user.image ? <Image src={user.image} alt='user' fill className='object-cover rounded-full'/> : <User/>}
         </div>
 
+        <AnimatePresence>
+          {open && 
+          
+         <motion.div
+         initial={{ opacity:0, y:-10,scale:0.95}}
+         animate={{ opacity:1, y:0,scale:1}}
+         transition={{ duration:0.6 }}
+         exit={{ opacity:0, y:-10,scale:0.95 }}
+
+         className="absolute right-0 mt-3 w-56 bg-white rounded-2xl shadow-xl border border-gray-200 p--3 z-999"
+         >
+          <div className='flex items-center gap-3 px-3 py-2 border-b border-gray-100'>
+            <div className='w-10 h-10 rounded-full bg-green-100 flex items-center justify-center overflow-hidden relative'>
+              {user.image ? <Image src={user.image} alt='user' fill className='object-cover rounded-full'/> : <User/>}
+            </div>
+            <div>
+                <div className='text-gray-800 font-semibold'>{user.name}</div>
+                <div className='text-xs text-gray-500 capitalize'>{user.role}</div>
+            </div>
+          </div>
+
+          <Link href={""} className='flex items-center gap-2 px-3 py-2 hover:bg-green-50 rounded-lg text-gray-700 font-medium onClick={()}=>setOpen(false)}>'>
+          <Package className='w-5 h-5 text-green-600'/>
+          <span>My Orders</span>
+          </Link>
+
+          <button className='flex items-center gap-2 w-full text-left px-3 py-3 hover:bg-red-50 rounded-lg text-gray-700 font-medium'>
+            <LogOut className='w-5 h-5 text-red-600'/>
+            Log Out
+            
+          </button>
+      </motion.div>
+         }
+        </AnimatePresence>
+
+        </div>
       </div>
       
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,9 +22,11 @@ async function Home() {
     return <EditRoleMobile />
     }
 
+    const plainUser = JSON.parse(JSON.stringify(user))
+
   return (
     <>
-      <Nav user={user} />
+      <Nav user={plainUser} />
     </>
   )
 }
