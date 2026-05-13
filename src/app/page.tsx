767ced565eae479632a0e0a3f1079c4506ea8ed9
mowlyray <mowlyray421@gmail.com>
2026-05-13 13:08:36 +0600

    update nav for admin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,41 @@
+import { auth } from '@/auth'
+import AdminDashboard from '@/components/AdminDashboard'
+import DeliveryBoy from '@/components/DeliveryBoy'
+import EditRoleMobile from '@/components/EditRoleMobile'
+import Nav from '@/components/Nav'
+import UserDashboard from '@/components/UserDashboard'
+import connectDb from '@/lib/db'
+import User from '@/models/user.model'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-function Home() {
+async function Home() {
+   await connectDb()
+  const session = await auth()
+
+  const user = await User.findById(session?.user?.id)
+
+  if(!user) {
+    redirect('/login')
+  }
+
+  const inComplete= !user.mobile || !user.role || (!user.mobile && user.role == "user")
+
+  if(inComplete) {
+    return <EditRoleMobile />
+    }
+
+    const plainUser = JSON.parse(JSON.stringify(user))
+
   return (
-    <div>
-      Home Page
-    </div>
+    <>
+      <Nav user={plainUser} />
+      {user.role=="user" ?(
+        <UserDashboard/>
+      ) : user.role== "admin" ? (
+        <AdminDashboard />
+      ) : <DeliveryBoy/>}
+    </>
   )
 }
 
