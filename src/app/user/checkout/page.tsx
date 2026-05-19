ae1aa4d294d67987e4fc06681a073f2d897ebc9f
mowlyray <mowlyray421@gmail.com>
2026-05-19 16:32:33 +0600

    create Mapview.tsx file

--- src/app/user/checkout/page.tsx
+++ src/app/user/checkout/page.tsx
@@ -3,7 +3,7 @@ import { RootState } from '@/redux/store'
 import { motion } from 'framer-motion'
 import { ArrowLeft, Building, Home, MapPin, Navigation, Phone, Search, User } from 'lucide-react'
 import { useRouter } from 'next/navigation'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Root } from 'react-dom/client'
 import { useSelector } from 'react-redux'
 
@@ -12,13 +12,34 @@ function Checkout() {
   const router=useRouter()
   const {userData} = useSelector((state:RootState) => state.user)
   const [address,setAddress] = useState({
-    fullName:userData?.name,
-    mobile:userData?.mobile,
+    fullName:"",
+    mobile:"",
     city:"",
     state:"",
     pinCode:"",
     fullAddress:""
   })
+
+  const [position,setPosition] = useState<[number,number]|null>(null)
+
+  useEffect(()=>{
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition((pos)=>{
+        const {latitude,longitude} = pos.coords
+        setPosition([latitude,longitude])
+      }
+      )
+    }
+  },[])
+
+  useEffect(()=>{
+    if(userData){
+       setAddress((prev)=>({...prev,fullName:userData?.name || ""}))
+      setAddress((prev)=>({...prev,mobile:userData?.mobile || ""}))
+    }
+  },[userData])
+
+
 
   return (
     <div className='w-[92%] md:w-[80%] py-10 mx-auto relative' >
@@ -57,7 +78,7 @@ function Checkout() {
                 <User className='absolute left-3 top-3 text-green-600' size={18}/>
                 <input
                   type="text"
-                  value={address.fullName} onChange={(e)=>setAddress({...address,fullName:e.target.value})}
+                  value={address.fullName} onChange={(e)=>setAddress((prev)=>({...prev,fullName:address.fullName}))}
                  className="bg-gray-50 border pl-10 w-full p-3 text-sm  rounded-lg"
                 />
               </div>
@@ -66,7 +87,7 @@ function Checkout() {
                 <Phone className='absolute left-3 top-3 text-green-600' size={18}/>
                 <input
                   type="text"
-                  value={address.mobile} onChange={(e)=>setAddress({...address,mobile:e.target.value})}
+                  value={address.mobile} onChange={(e)=>setAddress((prev)=>({...prev,mobile:address.mobile}))}
                   className="bg-gray-50 border pl-10 w-full p-3 text-sm  rounded-lg"
                 />
               </div>
@@ -75,7 +96,7 @@ function Checkout() {
                 <Home className='absolute left-3 top-3 text-green-600' size={18}/>
                 <input
                   type="text"
-                  value={address.fullAddress} placeholder='Enter your full address' onChange={(e)=>setAddress({...address,fullAddress:e.target.value})}
+                  value={address.fullAddress} placeholder='Enter your full address' onChange={(e)=>setAddress((prev)=>({...prev,fullAddress:address.fullAddress}))}
                   className="bg-gray-50 border pl-10 w-full p-3 text-sm  rounded-lg"
                 />
               </div>
@@ -87,7 +108,7 @@ function Checkout() {
                   type="text"
                   value={address.city}
                   placeholder='city' 
-                  onChange={(e)=>setAddress({...address,city:e.target.value})}
+                  onChange={(e)=>setAddress((prev)=>({...prev,city:address.city}))}
                   className="bg-gray-50 border pl-10 w-full p-3 text-sm  rounded-lg"
                 />
               </div>
@@ -98,7 +119,7 @@ function Checkout() {
                   type="text"
                   value={address.state}
                   placeholder='state' 
-                  onChange={(e)=>setAddress({...address,state:e.target.value})}
+                 onChange={(e)=>setAddress((prev)=>({...prev,state:address.state}))}
                   className="bg-gray-50 border pl-10 w-full p-3 text-sm  rounded-lg"
                 />
               </div>
@@ -109,10 +130,15 @@ function Checkout() {
                   type="text"
                   value={address.pinCode}
                   placeholder='pin code' 
-                  onChange={(e)=>setAddress({...address,pinCode:e.target.value})}
+                  onChange={(e)=>setAddress((prev)=>({...prev,pinCode:address.pinCode}))}
                   className="bg-gray-50 border pl-10 w-full p-3 text-sm  rounded-lg"
                 />
               </div>
+              </div>
+
+              <div className='flex gap-2 mt-3'>
+                <input type='text' placeholder='search city or area...' className='flex-1 border rounded-lg pp-3 text-sm focus:ring-2 focus:ring-green-500 outline-none'/>
+                <button className='bg-green-600 text-white px-5 rounded-lg hover:bg-green-600 transition-al font-medium'>Search</button>
               </div>
             </div>
 
