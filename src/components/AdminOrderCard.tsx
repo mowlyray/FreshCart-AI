335e79ac4ebcf59f6762601221aa9f73bdbef8b5
mowlyray <mowlyray421@gmail.com>
2026-05-25 12:27:01 +0600

    create admin get-orders api and AdminOrderCard component

---
+++ src/components/AdminOrderCard.tsx
@@ -0,0 +1,12 @@
+import { IOrder } from '@/models/order.model'
+import React from 'react'
+
+function AdminOrderCard({order}:{order:IOrder}) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AdminOrderCard
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -73,7 +73,7 @@ function Nav({user}:{user:IUser}) {
           <Link  href={"/admin/add-grocery"} className='flex items-center gap-3 p-3 rounded-lg bg-white/10 hover:bg-white/20 hover:pl-4 transition-all'><PlusCircle className="w-5 h-5" /> Add Grocery</Link>
           <Link  href={""} className='flex items-center gap-3 p-3 rounded-lg bg-white/10 hover:bg-white/20 hover:pl-4 transition-all'><Boxes className="w-5 h-5" /> View Grocery</Link>
           
-          <Link href={""} className='flex items-center gap-3 p-3 rounded-lg bg-white/10 hover:bg-white/20 hover:pl-4 transition-all'><ClipboardCheck className="w-5 h-5" />  Manage Orders</Link>
+          <Link href={"/admin/manage-orders"} className='flex items-center gap-3 p-3 rounded-lg bg-white/10 hover:bg-white/20 hover:pl-4 transition-all'><ClipboardCheck className="w-5 h-5" />  Manage Orders</Link>
         </div>
 
         <div className='my-5 border-t border-white/20'></div>
@@ -120,7 +120,7 @@ function Nav({user}:{user:IUser}) {
           <Link  href={"/admin/add-grocery"} className='flex items-center gap-2 bg-white text-green-700 font-semibold px-4 py-2 rounded-full hover:bg-green-100 transition-all'><PlusCircle className="w-5 h-5" /> Add Grocery</Link>
           <Link  href={""} className='flex items-center gap-2 bg-white text-green-700 font-semibold px-4 py-2 rounded-full hover:bg-green-100 transition-all'><Boxes className="w-5 h-5" /> View Grocery</Link>
           
-          <Link href={""} className='flex items-center gap-2 bg-white text-green-700 font-semibold px-4 py-2 rounded-full hover:bg-green-100 transition-all'><ClipboardCheck className="w-5 h-5" />  Manage Orders</Link>
+          <Link href={"/admin/manage-orders"} className='flex items-center gap-2 bg-white text-green-700 font-semibold px-4 py-2 rounded-full hover:bg-green-100 transition-all'><ClipboardCheck className="w-5 h-5" />  Manage Orders</Link>
         </div>
 
         <div className="md:hidden bg-white rounded-full w-10 h-10 p-2 flex items-center  shadow-md" 
