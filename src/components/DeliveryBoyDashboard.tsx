0f9516d73d06da3f97063460da8d10ace51d5951
mowlyray <mowlyray421@gmail.com>
2026-05-30 13:22:19 +0600

    creating Assignment Accept Api

--- src/components/DeliveryBoyDashboard.tsx
+++ src/components/DeliveryBoyDashboard.tsx
@@ -1,59 +1,75 @@
-'use client'
-import { getSocket } from '@/lib/socket'
-import  { IDeliveryAssignment } from '@/models/deliveryAssignment.model'
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-
+"use client";
+import { getSocket } from "@/lib/socket";
+import { IDeliveryAssignment } from "@/models/deliveryAssignment.model";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function DeliveryBoyDashboard() {
-    const [assignments,setAssignments]=useState<any[]>([])
+  const [assignments, setAssignments] = useState<any[]>([]);
 
-    useEffect(()=>{
-        const fetchAssignments=async ()=>{
-            try {
-                const result=await axios.get("/api/delivery/get-assignments")
-                setAssignments(result.data)
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        fetchAssignments()
-    },[])
+  useEffect(() => {
+    const fetchAssignments = async () => {
+      try {
+        const result = await axios.get("/api/delivery/get-assignments");
+        setAssignments(result.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchAssignments();
+  }, []);
 
-    useEffect(():any=>{
-        const socket=getSocket()
+  useEffect((): any => {
+    const socket = getSocket();
 
-        socket.on("new-assignment",(deliveryAssignment)=>{
-            setAssignments((prev)=>[...prev,deliveryAssignment])
-        })
-        return ()=>socket.off("new-assignment")
-    },[])
+    socket.on("new-assignment", (deliveryAssignment) => {
+      setAssignments((prev) => [...prev, deliveryAssignment]);
+    });
+    return () => socket.off("new-assignment");
+  }, []);
+
+  const handleAccept=async (id:string)=>{
+    try {
+        const result=await axios.get(`/api/delivery/assignment/${id}/accept-assignment`)
+        console.log(result)
+    } catch (error) {
+        console.log(error)
+
+    }
+  }
 
 
-    
+
   return (
-    <div className='w-full min-h-screen bg-gray-50 p-4'>
-        <div className='max-w-3xl mx-auto'>
-            <h2 className='text-2xl font-bold mt-[120px] mb-[30px]'>Delivery Assignment</h2>
+    <div className="w-full min-h-screen bg-gray-50 p-4">
+      <div className="max-w-3xl mx-auto">
+        <h2 className="text-2xl font-bold mt-[120px] mb-[30px]">
+          Delivery Assignment
+        </h2>
 
-            {assignments.map(a=>(
-                <div key={a._id} className='p-5 bg-white rounded-xl shadow mb-4 border'>
-                    <p><b>Order Id</b>#{a?.order._id.slice(-6)}</p>
-                    <p className='text-gray-600'>{a.order.address.fullAddress}</p>
+        {assignments.map((a) => (
+          <div
+            key={a._id}
+            className="p-5 bg-white rounded-xl shadow mb-4 border"
+          >
+            <p>
+              <b>Order Id</b>#{a?.order._id.slice(-6)}
+            </p>
+            <p className="text-gray-600">{a.order.address.fullAddress}</p>
 
-                    <div className='flex gap-3 mt-4'>
-                        <button className='flex-1 bg-green-600 text-white py-2 rounded-lg'>Accept</button>
-                        <button className='flex-1 bg-red-600 text-white py-2 rounded-lg'>Reject</button>
-                    </div>
-
-                </div>
-                
-            ))}
-
-        </div>
-      
+            <div className="flex gap-3 mt-4">
+              <button className="flex-1 bg-green-600 text-white py-2 rounded-lg" onClick={()=>handleAccept(a._id)}>
+                Accept
+              </button>
+              <button className="flex-1 bg-red-600 text-white py-2 rounded-lg">
+                Reject
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default DeliveryBoyDashboard
+export default DeliveryBoyDashboard;
